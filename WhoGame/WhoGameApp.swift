2a122dd6129da7a07db4e611e58f4e52a6f9e353
iMarishin <move_up@icloud.com>
2021-12-05 20:43:23 +0500

    Initial Commit

---
+++ WhoGame/WhoGameApp.swift
@@ -0,0 +1,20 @@
+//
+//  WhoGameApp.swift
+//  WhoGame
+//
+//  Created by Иван Маришин on 05.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct WhoGameApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
