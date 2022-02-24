7b5b076117afd96b7636e6b45c136e87a310a678
iMarishin <move_up@icloud.com>
2022-02-24 20:31:56 +0500

    some fix

--- WhoGame/MainView/TermsPrivacyView.swift
+++ WhoGame/MainView/TermsPrivacyView.swift
@@ -70,7 +70,7 @@ Changes to This Privacy Policy
 I may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically for any changes. I will notify you of any changes by posting the new Privacy Policy on this page.
 This policy is effective as of 2022-02-20
 Contact Us
-If you have any questions or suggestions about my Privacy Policy, do not hesitate to contact me at iimarishin@gmail.com.
+If you have any questions or suggestions about my Privacy Policy, do not hesitate to contact me at whogame.im@gmail.com.
 
 """)
                 .padding(.bottom, 16)
@@ -91,7 +91,7 @@ Changes to This Terms and Conditions
 I may update our Terms and Conditions from time to time. Thus, you are advised to review this page periodically for any changes. I will notify you of any changes by posting the new Terms and Conditions on this page.
 These terms and conditions are effective as of 2022-02-20
 Contact Us
-If you have any questions or suggestions about my Terms and Conditions, do not hesitate to contact me at iimarishin@gmail.com.
+If you have any questions or suggestions about my Terms and Conditions, do not hesitate to contact me at whogame.im@gmail.com.
 """)
                 .padding(.bottom, 16)
         }
